b2118b5d6d400facf06d4a49bdc23b7e6afa8209
nczhu <n@pragmatic.vc>
2022-05-03 19:25:34 +0100

    fix: use fr finite field

--- benches/bench_lib.rs
+++ benches/bench_lib.rs
@@ -6,23 +6,21 @@ extern crate lazy_static;
 extern crate test;
 use test::Bencher;
 
+use ark_bls12_381::Fr;
 use freivald;
 use ndarray::Array2;
-use ndarray_rand::rand_distr::Uniform;
-use ndarray_rand::RandomExt;
 
 lazy_static! {
-	static ref MATRIX_A: Array2<u128> = Array2::random((300, 300), Uniform::new(0, 10));
-	static ref MATRIX_B: Array2<u128> = Array2::<u128>::ones((300, 300));
-	static ref MATRIX_A_DOT_B: Array2<u128> = MATRIX_A.dot(&Array2::<u128>::ones((300, 300)));
+	static ref MATRIX_A: Array2<Fr> = Array2::<Fr>::ones((50, 50));
+	static ref MATRIX_A_DOT_A: Array2<Fr> = MATRIX_A.dot(&Array2::<Fr>::ones((50, 50)));
 }
 
 #[bench]
 fn dumb_verify_test(b: &mut Bencher) {
-	b.iter(|| freivald::dumb_verify(&MATRIX_A, &MATRIX_B, &MATRIX_A_DOT_B));
+	b.iter(|| freivald::dumb_verify(&MATRIX_A, &MATRIX_A, &MATRIX_A_DOT_A));
 }
 
 #[bench]
 fn freivald_verify_test(b: &mut Bencher) {
-	b.iter(|| freivald::freivald_verify(&MATRIX_A, &MATRIX_B, &MATRIX_A_DOT_B));
+	b.iter(|| freivald::freivald_verify(&MATRIX_A, &MATRIX_A, &MATRIX_A_DOT_A));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,53 @@
-#![allow(arithmetic_overflow)]
+use ark_bls12_381::Fr;
 use ndarray::{Array1, Array2};
 use rand::Rng;
 use std::iter::Iterator;
+
 struct Univar {
-    product: u128,
-    r: u128,
+    product: Fr,
+    r: Fr,
 }
 
 impl Univar {
-    fn new(r: u128) -> Self {
-        Univar { product: 1, r }
+    fn new(r: Fr) -> Self {
+        Univar {
+            product: 1.into(),
+            r,
+        }
     }
 }
 
 impl Iterator for Univar {
-    type Item = u128;
+    type Item = Fr;
     fn next(&mut self) -> Option<Self::Item> {
-        self.product = self.product.wrapping_mul(self.r);
+        self.product *= self.r;
         Some(self.product)
     }
 }
 
-pub fn get_r() -> u128 {
+pub fn get_r() -> Fr {
     let mut rng = rand::thread_rng();
-    rng.gen()
+    let r: Fr = rng.gen();
+    r
 }
 
-pub fn get_vec(r: u128, n: usize) -> Array1<u128> {
+pub fn get_vec(r: Fr, n: usize) -> Array1<Fr> {
     let var = Univar::new(r);
     var.take(n).collect()
 }
 
-pub fn freivald_verify(a: &Array2<u128>, b: &Array2<u128>, c: &Array2<u128>) -> bool {
+pub fn freivald_verify(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
     assert!(check_matrix_dimensions(a, b, c));
     let v = get_vec(get_r(), c.ncols());
     a.dot(&b.dot(&v)) == c.dot(&v)
 }
 
-pub fn dumb_verify(a: &Array2<u128>, b: &Array2<u128>, c: &Array2<u128>) -> bool {
+pub fn dumb_verify(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
     assert!(check_matrix_dimensions(a, b, c));
     a.dot(b) == c
 }
 
-pub fn check_matrix_dimensions(a: &Array2<u128>, b: &Array2<u128>, c: &Array2<u128>) -> bool {
+pub fn check_matrix_dimensions(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
     a.nrows() == b.ncols()
         && a.ncols() == b.nrows()
         && c.nrows() == a.nrows()
