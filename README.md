c86d736f32d8be7c4b9005694b4392278d3e1c3f
nczhu <n@pragmatic.vc>
2022-05-14 08:58:23 -0700

    refactor: use repo for all impls

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# Freivald-Rust
+# ZK Algorithms
 
-A Rust implementation of Freivald's algorithm on matrix multiplication verification.
+Rust implementation of algorithms in Thaler's Proofs, Arguments and ZK study group. 
+
+###
+1. `Freivald`: Freivald's algorithm on matrix multiplication verification
+2. `Lagrange`: Multilinear extension (MLE) via Lagrange Interpolation
 
 ### Get Started
 ```rust
 cargo test
 cargo bench
+```
+
+To run a specific test:
+```rust 
+cargo test --test test_name.rs
+cargo bench --bench bench_name.rs
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,5 @@
-use ark_bls12_381::Fr;
-use ndarray::{Array1, Array2};
-use rand::Rng;
-use std::iter::Iterator;
+// Import modules
+mod freivald;
 
-struct Univar {
-    product: Fr,
-    r: Fr,
-}
-
-impl Univar {
-    fn new(r: Fr) -> Self {
-        Univar {
-            product: 1.into(),
-            r,
-        }
-    }
-}
-
-impl Iterator for Univar {
-    type Item = Fr;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.product *= self.r;
-        Some(self.product)
-    }
-}
-
-pub fn get_r() -> Fr {
-    let mut rng = rand::thread_rng();
-    let r: Fr = rng.gen();
-    r
-}
-
-pub fn get_vec(r: Fr, n: usize) -> Array1<Fr> {
-    let var = Univar::new(r);
-    var.take(n).collect()
-}
-
-pub fn freivald_verify(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
-    assert!(check_matrix_dimensions(a, b, c));
-    let v = get_vec(get_r(), c.ncols());
-    a.dot(&b.dot(&v)) == c.dot(&v)
-}
-
-pub fn dumb_verify(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
-    assert!(check_matrix_dimensions(a, b, c));
-    a.dot(b) == c
-}
-
-pub fn check_matrix_dimensions(a: &Array2<Fr>, b: &Array2<Fr>, c: &Array2<Fr>) -> bool {
-    a.nrows() == b.ncols()
-        && a.ncols() == b.nrows()
-        && c.nrows() == a.nrows()
-        && c.ncols() == b.ncols()
-}
+// Re-export modules as a part of lib
+pub use crate::freivald::*;
