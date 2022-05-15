58b638ce20f6bd18825ef49184b4984e14e8146e
nczhu <n@pragmatic.vc>
2022-05-15 08:12:13 -0700

    chore: comment clean up

--- src/lagrange.rs
+++ src/lagrange.rs
@@ -1,6 +1,3 @@
-// TODO use Fr
-use std::collections::HashMap;
-
 // One step in chi
 pub fn chi_step(w: bool, x: i128) -> i128 {
 	x * i128::from(w) + (1 - x) * (1 - i128::from(w))
@@ -17,9 +14,7 @@ pub fn n_to_vec(i: usize, n: usize) -> Vec<bool> {
 	x
 }
 
-// Computes Chi_w(r) for any w, O(log n) operations
-// w: in {0,1}^v
-// r: in {p}^v, e.g. F_5
+// Computes Chi_w(r) for all w, O(log n) operations
 pub fn chi_w(w: &Vec<bool>, r: &Vec<i128>) -> i128 {
 	assert_eq!(w.len(), r.len());
 	let product: i128 = w
@@ -30,13 +25,7 @@ pub fn chi_w(w: &Vec<bool>, r: &Vec<i128>) -> i128 {
 	product
 }
 
-// Given a vector `r` in (F_logn)
-// Given evaluation table of f(w), for all w in {0,1}^v
-// Stored in vec, as 000, 001, 010, etc.
-// Given n = 2^v
-// Given p: the field size, e.g. 5
-// Compute f~(r) in linear (slow) way
-// Output: evaluation in field F_p
+// Calculating the slow way, for benchmarking
 pub fn slow_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
 	assert_eq!(r.len() as f64, (fw.len() as f64).sqrt());
 	let sum: i128 = fw
@@ -47,10 +36,7 @@ pub fn slow_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
 	sum % p
 }
 
-// --------------------
-// Similar to slow_mle, but recursive:
-// f~(r) <- f~(r) + f(w) * chi_w(r)
-// Brings improvements in memory usage, storing just O(log n) elements
+// Lemma 3.7
 pub fn stream_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
 	recurse(fw, r, 2usize.pow(r.len() as u32)) % p
 }
@@ -62,54 +48,30 @@ pub fn recurse(fw: &Vec<i128>, r: &Vec<i128>, n: usize) -> i128 {
 	}
 }
 
-// --------------------
-
-// Procedure is v stages
-// Create chi table, storing w_000, w_001, w_010, so on.
-// Memoizing each iteration, e.g.
-// w_0 contributes to calculation of w_00, w_01
-// w_1 contributes to calcluation of w_10, 1_11, so on
+// Lemm 3.8
 pub fn dynamic_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
-	let chi_lookup = memoize(r, r.len()); // vec of length n = 2^v
-
+	let chi_lookup = memoize(r, r.len());
 	let result: i128 = fw
 		.iter()
 		.zip(chi_lookup.iter())
 		.map(|(left, right)| left * right)
 		.sum();
 	result % p
-	// Step 2: get inner product of f(w) and chi(r), i.e. just fw dot lookup table.
 }
 
-// v=0, recurse & implicitly "memoize" the redundant calculations
-// track w as a string..."00" "000" are different...
-pub fn memoize(
-	r: &Vec<i128>,
-	v: usize, // start 2
-) -> Vec<i128> {
+pub fn memoize(r: &Vec<i128>, v: usize) -> Vec<i128> {
 	match v {
 		1 => {
-			println!("Matched v=1 (once)");
-			let basevec = vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])];
-			println!("Base vec: {:?}", basevec);
-			basevec
+			vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])]
 		}
-		_ => {
-			println!("Matched v={:?}", v);
-			// or just get the prev vec and flatten it... vec![prev vec.flatten]
-			// each vector of previous vec* chim, prev_vec * chi.
-			// explode vector out into bigger vec...
-			let vec0: Vec<i128> = memoize(r, v - 1)
-				.iter()
-				.flat_map(|val| {
-					[
-						val * chi_step(false, r[v - 1]),
-						val * chi_step(true, r[v - 1]),
-					]
-				})
-				.collect();
-			println!("Vec now looks like: {:?} (once)", vec0);
-			vec0
-		}
+		_ => memoize(r, v - 1)
+			.iter()
+			.flat_map(|val| {
+				[
+					val * chi_step(false, r[v - 1]),
+					val * chi_step(true, r[v - 1]),
+				]
+			})
+			.collect(),
 	}
 }
--- tests/test_lagrange.rs
+++ tests/test_lagrange.rs
@@ -2,7 +2,6 @@
 extern crate lazy_static;
 
 use rstest::rstest;
-use std::collections::HashMap;
 use thaler::lagrange;
 
 lazy_static! {
@@ -11,7 +10,13 @@ lazy_static! {
 	static ref R_1: Vec<i128> = Vec::from([0, 2]);
 	static ref R_2: Vec<i128> = Vec::from([3, 4]);
 	static ref R_3: Vec<i128> = Vec::from([4, 1]);
-	// Test with v=3 as well...
+	// Larger v tests
+	static ref R_4: Vec<i128> = Vec::from([0, 0, 0]);
+	static ref R_4_CHI: Vec<i128> = Vec::from([1, 0, 0, 0, 0, 0, 0, 0]);
+	static ref R_5: Vec<i128> = Vec::from([1, 2, 3]);
+	static ref R_5_CHI: Vec<i128> = Vec::from([0, 0, 0, 0, 2, -3, -4, 6]);
+	static ref R_6: Vec<i128> = Vec::from([5, 5, 5]);
+	static ref R_6_CHI: Vec<i128> = Vec::from([-64, 80, 80, -100, 80, -100, -100, 125]);
 }
 
 #[rstest]
@@ -47,11 +52,19 @@ fn stream_lagrange_test(
 #[case(&F_2, &R_1, 3, 5)]
 #[case(&F_2, &R_2, 4, 5)]
 #[case(&F_2, &R_3, 0, 5)]
-fn memoize_test(
+fn dynamic_mle_test(
 	#[case] fw: &Vec<i128>,
 	#[case] r: &Vec<i128>,
 	#[case] expected: i128,
 	#[case] p: i128,
 ) {
 	assert_eq!(lagrange::dynamic_mle(fw, r, p), expected);
+}
+
+#[rstest]
+#[case(&R_4, &R_4_CHI)]
+#[case(&R_5,&R_5_CHI)]
+#[case(&R_6, &R_6_CHI)]
+fn memoize_test(#[case] r: &Vec<i128>, #[case] expected: &Vec<i128>) {
+	assert_eq!(lagrange::memoize(r, r.len()), *expected);
 }
